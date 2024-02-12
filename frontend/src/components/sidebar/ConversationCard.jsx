eeb71561b90954f9d642167cd633e783e31aeee2
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-11 22:49:57 -0500

    show ConversationCard by user

--- frontend/src/components/sidebar/ConversationCard.jsx
+++ frontend/src/components/sidebar/ConversationCard.jsx
@@ -1,31 +1,25 @@
-// import userConversation from "../../zustand/useConversation";
-
-// const ConversationCard = ({ user, lastIdx }) => {
-//   const { selectedConversation, setSelectedConversation } = userConversation();
-//   const isSelected = selectedConversation?._id === user._id;
-//   return (
-//     <>
-//       <div
-//         className={`group flex cursor-pointer items-center gap-5 rounded-md p-2 duration-500 hover:bg-sky-500 ${isSelected ? "bg-sky-500 text-white" : ""}`}
-//         onClick={() => setSelectedConversation(user)}
-//       >
-//         {/* AVATAR */}
-//         <div className="avatar online">
-//           <div className="w-12 rounded-full">
-//             <img src={user.profilePhoto} alt="user avatar" />
-//           </div>
-//         </div>
-//         <div className="flex items-center duration-300 group-hover:text-white">
-//           <p>{user.fullName}</p>
-//         </div>
-//       </div>
-//       {!lastIdx && <div className="divider my-0 h-1 py-0" />}
-//     </>
-//   );
-// };
-
-const ConversationCard = ()=>{
-  return <div className="text-xl">Conversation Card</div>
-}
-
+import useConversation from "../../zustand/useConversation";
+const ConversationCard = ({ conversation, lastIdx }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === conversation._id;
+  return (
+    <div>
+      <div
+        className={`group flex cursor-pointer items-center gap-5 rounded-md p-2 duration-500 hover:bg-sky-500 ${isSelected ? "bg-sky-500 text-white" : ""}`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
+        {/* AVATAR */}
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePhoto} alt="user avatar" />
+          </div>
+        </div>
+        <div className="flex items-center duration-300 group-hover:text-white">
+          <p>{conversation.fullName}</p>
+        </div>
+      </div>
+      {!lastIdx && <div className="divider my-0 h-1 py-0" />}
+    </div>
+  );
+};
 export default ConversationCard;
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,30 +1,48 @@
 // import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import ConversationCard from "./ConversationCard";
-
+// import useGetConversation from "../../hooks/useGetConversation";
+import { useEffect, useState } from "react";
 // import { useAuthContext } from "../../context/AuthContext";
 // import { getAllUsers } from "../../hooks/conversation.hook";
 
 const Conversations = () => {
-  // const [users, setUsers] = useState(null);
-  // const { authUser } = useAuthContext();
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const data = await getAllUsers(authUser.token);
-  //     setUsers(data.users);
-  //   };
-  //   fetchData();
-  // }, [authUser]);
-
+  const [conversations, setConversation] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    try {
+      setLoading(true);
+      const fetchData = async () => {
+        const res = await fetch("/api/users");
+        const data = await res.json();
+        setConversation(data.users);
+      };
+      fetchData();
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  console.log(conversations);
   return (
     <div className="flex-1 overflow-auto">
-      {/* {users?.map((user, idx) => (
-        <ConversationCard
-          key={user._id}
-          user={user}
-          lastIdx={idx === users.length - 1}
-        />
-      ))} */}
-      <ConversationCard />
+      {loading ? (
+        <div className="flexCenter h-full flex-col gap-10">
+          <progress className="progress w-56"></progress>
+          <p className="text-xl font-semibold text-white">Loading Data</p>
+        </div>
+      ) : (
+        <>
+          {conversations?.map((conversation) => (
+            <ConversationCard
+              conversation={conversation}
+              key={conversation._id}
+              lastIdx={Conversations.length - 1}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
