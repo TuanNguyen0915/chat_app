f9077158dd1f936e69fb7b1d7021480593941432
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-12 22:32:13 -0500

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import path from 'path'
 import 'dotenv/config'
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import './config/database.js'
+import connectToMongoDB from './config/database.js'
+
 import { app, server } from './socket/socket.js'
 import { router as authRouter } from './routes/auth.route.js'
 import { router as messageRouter } from './routes/message.route.js'
@@ -13,15 +14,16 @@ const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
 app.use('/api/auth', authRouter)
 app.use('/api/messages', messageRouter)
 app.use('/api/users', userRouter)
 
-app.get("*", (req,res) => {
-  res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
 server.listen(port, () => {
+  connectToMongoDB()
   console.log(`Server is running at port ${port}`)
 })
