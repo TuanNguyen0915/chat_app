8208316d58dfd9852e8a7905fff0587c9fe82909
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-12 21:28:46 -0500

    fix bug for sending message

--- frontend/src/hooks/useGetMessages.js
+++ frontend/src/hooks/useGetMessages.js
@@ -1,16 +1,47 @@
-import toast from "react-hot-toast"
+// import toast from "react-hot-toast"
 
-const useGetMessages = async (conversationId) => {
-  try {
-    const res = await fetch(`/api/messages/${conversationId}`)
-    const data = await res.json()
-    if(data.error) {
-      toast.error(data.error)
-    }
-    return data
-  } catch (error) {
-    toast.error(error.message)
-  }
-}
+// const useGetMessages = async (conversationId) => {
+//   try {
+//     const res = await fetch(`/api/messages/${conversationId}`)
+//     const data = await res.json()
+//     if(data.error) {
+//       toast.error(data.error)
+//     }
+//     return data
+//   } catch (error) {
+//     toast.error(error.message)
+//   }
+// }
 
-export default useGetMessages
+// export default useGetMessages
+
+import { useEffect, useState} from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false)
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoading(true)
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation._id}`);
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+        // console.log(data.messages)
+        setMessages(data.messages);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false)
+      }
+    };
+
+    if (selectedConversation?._id) getMessages();
+  }, [selectedConversation?._id, setMessages]);
+
+  return { messages, loading};
+};
+export default useGetMessages;
--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -1,17 +1,32 @@
-const useSendMessage = async (message, conversationId) => {
-  try {
-    const res = await fetch(`/api/messages/send/${conversationId}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({message}),
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+import { useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
 
+const useSendMessage = () => {
+	const [loading, setLoading] = useState(false);
+	const { messages, setMessages, selectedConversation } = useConversation();
+
+	const sendMessage = async (message) => {
+		setLoading(true);
+		try {
+			const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ message }),
+			});
+			const data = await res.json();
+			if (data.error) throw new Error(data.error);
+
+			setMessages([...messages, data]);
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return { sendMessage, loading };
+};
 export default useSendMessage;
