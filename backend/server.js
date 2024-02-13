307d9ab7cc9279be995b16fe6cfd6505660aaa61
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-12 21:51:02 -0500

    fixed app structure

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import 'dotenv/config'
 import express from 'express'
 import cookieParser from 'cookie-parser'
@@ -8,13 +9,18 @@ import { router as messageRouter } from './routes/message.route.js'
 import { router as userRouter } from './routes/user.router.js'
 
 const port = process.env.PORT || 3000
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 app.use('/api/auth', authRouter)
 app.use('/api/messages', messageRouter)
 app.use('/api/users', userRouter)
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+})
 
 server.listen(port, () => {
   console.log(`Server is running at port ${port}`)
