f48f737e042920a0d751668b730d637fd794d781
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-12 22:05:22 -0500

    Deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
-const db = mongoose.connection
-mongoose.connect(process.env.MONGO_URI)
-db.on('connected', ()=> {
-  console.log(`Mongo connect at ${db.name}`)
-})
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
+};
+
+export default connectToMongoDB;
